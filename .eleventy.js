d92a2f2e6c93afb5378bcf16bc1e558f14a4a04a
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-05-29 04:51:10 -0500

    Start adding content and add scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,41 @@
+const path = require("node:path");
+const sass = require("sass");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addTemplateFormats("scss")
 
   eleventyConfig.addWatchTarget("src/css/");
+
+  eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+
+		// opt-out of Eleventy Layouts
+		useLayouts: false,
+
+		compile: async function (inputContent, inputPath) {
+			let parsed = path.parse(inputPath);
+			// Don’t compile file names that start with an underscore
+			if(parsed.name.startsWith("_")) {
+				return;
+			}
+
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsed.dir || ".",
+					this.config.dir.includes,
+				]
+			});
+
+			// Map dependencies for incremental builds
+			this.addDependencies(inputPath, result.loadedUrls);
+
+			return async (data) => {
+				return result.css;
+			};
+		},
+	});
   
   return {
     dir: {
--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 * npm init -y
 * npm i -D @11ty/eleventy
 * npx @11ty/eleventy --serve
+* npm install sass
 
 
 ### Resources
 * https://www.11ty.dev
 * https://www.youtube.com/watch?v=uzM5lETc6Sg&list=PLtLXFsdHI8JTwScHvB924dY3PNwNJjjuW
+* https://www.11ty.dev/docs/projects/
